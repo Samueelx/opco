e2576fd9fe8d2194a41993c20df65268615ddc10
Samueelx <gsammygitau@gmail.com>
2026-02-27 16:35:34 +0300

    change request hostname

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,24 +1,18 @@
 import axios from "axios";
 
+const getBaseURL = () => {
+  const hostname = window.location.hostname;
+  // If running locally on any dev machine, use localhost
+  if (hostname === "localhost" || hostname === "127.0.0.1") {
+    return "http://localhost:8800/api";
+  }
+  // On the live server, use the same hostname the browser connected to
+  return `http://${hostname}:8800/api`;
+};
+
 const apiRequest = axios.create({
-  // baseURL: "http://172.23.23.53:8800/api",
-  baseURL: "http://localhost:8800/api",
+  baseURL: getBaseURL(),
   withCredentials: true,
 });
 
 export default apiRequest;
-
-// import axios from 'axios'
-
-// const getBaseURL = ()=> {
-//   const isLocal = window.location.hostname === "localhost"
-  
-//   return isLocal ? "http://localhost:8800/api" : "http://172.0.0.1:8800/api"
-// }
-
-// const apiRequest = axios.create({
-//   baseURL: getBaseURL(),
-//   withCredentials: true
-// })
-
-// export default apiRequest;
